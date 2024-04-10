a22bd9abe3dbfe48df51ad86b8b772229824cf2e
Nane Kratzke <nane.kratzke@th-luebeck.de>
2024-04-10 07:50:08 +0000

    app.py

--- app/app.py
+++ app/app.py
@@ -1,4 +1,13 @@
 import streamlit as st
 
-st.title('Your first Streamlit app')
+st.markdown("""
+<style>
+.block-container { text-align: center; }
+img { transform: rotate(-15deg); padding: 7em; }
+</style>
+""", unsafe_allow_html=True)
+
+st.title('3, 2, 1, ... Lift off')
 st.image("rocket.png")
+st.caption("Your first Streamlit app is running.")
+st.write("Now, make it your own!")
