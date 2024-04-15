77472afefbe99ad47a72fc0ce42b1044554388b5
Weiss, Marvin <marvin.weiss@stud.th-luebeck.de>
2024-04-15 15:29:16 +0000

    Update 2 files
    - /app/app.py
    - /.gitlab-ci.yml

--- app/app.py
+++ app/app.py
@@ -1,13 +1,6 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-st.markdown("""
-<style>
-.block-container { text-align: center; }
-img { transform: rotate(-15deg); padding: 7em; }
-</style>
-""", unsafe_allow_html=True)
+st.title('Uber pickups in NYC')
 
-st.title('3, 2, 1, ... Lift off')
-st.image("rocket.png")
-st.caption("Your first Streamlit app is running.")
-st.write("Now, make it your own!")
